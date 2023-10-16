15cfb7871396f701e74456825dd768a5dfd35f17
jamesnyakush <jnyakush99@gmail.com>
2023-10-16 10:00:16 +0300

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by James Nyakundi on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
